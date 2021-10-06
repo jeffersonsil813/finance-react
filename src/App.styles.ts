a501600bf843d6e96c60b4eb723cab35880198da
jeffersonsil813 <jefferson.sil813@gmail.com>
2021-10-06 14:50:03 -0300

    Tipagem dos dados padrões em data/Items.ts

---
+++ src/App.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+  background-color: var(--purple-color);
+  height: 150px;
+  text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  padding-top: 30px;
+`;
+
+export const Body = styled.div`
+  margin: 0 auto;
+  max-width: 980px;
+  margin-bottom: 50px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as C from "./App.styles";
+
+export function App() {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+
+      <C.Body>...</C.Body>
+    </C.Container>
+  );
+}
